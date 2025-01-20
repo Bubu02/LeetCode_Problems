9cc9baf2d01dda03ffc10374fa6b4967b6cd68a1
Satya Prakash Mohanty <120954412+Bubu02@users.noreply.github.com>
2025-01-20 12:13:09 +0530

    Time: 1587 ms (11.24%), Space: 0B (100%) - LeetHub

---
+++ 1882-the-number-of-employees-which-report-to-each-employee/1882-the-number-of-employees-which-report-to-each-employee.sql
@@ -0,0 +1,19 @@
+# Write your MySQL query statement below
+SELECT e1.employee_id,
+        e1.name,
+        COUNT(e2.reports_to) AS reports_count,
+        ROUND(AVG(e2.age),0) AS average_age
+FROM Employees e1
+JOIN Employees e2
+ON e1.employee_id=e2.reports_to
+GROUP BY e1.employee_id,e1.name
+ORDER BY e1.employee_id
+-- HAVING e1.reports_to<>'null'
+
+-- SELECT *
+-- FROM Employees e
+-- WHERE reports_to IN (
+--     SELECT reports_to
+--     FROM Employees
+--     WHERE reports_to<>'null'
+-- )
