39d666a0b6eb6bbf987e14b8c7a2b7c8081d6170
Satya Prakash Mohanty <120954412+Bubu02@users.noreply.github.com>
2025-01-20 12:54:32 +0530

    Time: 2916 ms (5%), Space: 0B (100%) - LeetHub

--- 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
+++ 1942-primary-department-for-each-employee/1942-primary-department-for-each-employee.sql
@@ -1,11 +1,4 @@
 # Write your MySQL query statement below
--- SELECT employee_id,
---         department_id,
---         CASE WHEN COUNT(employee_id)>1 THEN 'Y'
---         ELSE 'N' END AS primary_flag
--- FROM Employee
--- GROUP BY employee_id
-
 SELECT employee_id, department_id
 FROM Employee
 WHERE (primary_flag='Y'AND employee_id IN(
