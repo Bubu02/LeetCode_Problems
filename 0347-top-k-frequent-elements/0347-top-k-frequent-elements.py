67b5f18b86119b764e2984419eaf585e59b75388
Satya Prakash Mohanty <120954412+Bubu02@users.noreply.github.com>
2026-06-08 22:28:19 +0530

    Time: 6 ms (56.47%), Space: 22.9 MB (61.71%) - LeetHub

--- 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
+++ 0347-top-k-frequent-elements/0347-top-k-frequent-elements.py
@@ -2,6 +2,6 @@ from collections import Counter
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
         counted_nums = Counter(nums)
-        most_frequent_k_key = [key for key, counted_nums in counted_nums.most_common(k)]
+        most_frequent_k_key = [key for key, count in counted_nums.most_common(k)]
         return most_frequent_k_key
         
